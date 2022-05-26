befd53ddf59dcaac2b38e5de5c1a44612a1f5894
sxarism <stauros411@gmail.com>
2022-05-26 16:52:42 +0300

    Move docker-compose and bash files outside src folder

--- cleanup_docker_containers.sh
+++ cleanup_docker_containers.sh
@@ -2,5 +2,5 @@
 
 docker-compose down --rmi all --remove-orphans || true
 echo "May need to give password to remove database and migrations"
-sudo rm -R data/
-sudo rm -R evolvefg/netapp_endpoint/migrations/*
+sudo rm -R ./src/data/
+sudo rm -R ./src/evolvefg/netapp_endpoint/migrations/*
