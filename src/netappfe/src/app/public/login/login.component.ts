c30554e94f2b9aad8f6a56a781d80f5f71aab514
Katerina <katerinagiann98@gmail.com>
2022-10-26 11:51:46 +0300

    First commit, trying the login functionality

--- src/netappfe/src/app/public/login/login.component.ts
+++ src/netappfe/src/app/public/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -9,16 +10,12 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.scss', '../public.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   form: FormGroup;
+  errorMessage=false;
+  formData!: FormGroup;
+  isSubmitted: boolean;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-    ) { 
-
-    }
+  constructor(private formBuilder: FormBuilder,private authService: AuthService,private router: Router) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -27,16 +24,18 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  submit() {
+  submit() : void {
     const data = this.form.getRawValue()
-
+    this.isSubmitted = true;
     if (data.username == 'admin' && data.password == 'admin'){
       console.log('success')
+      // console.log('Is Login Success: ' + data);
       this.router.navigate(['/dashboard'])
     }
     else{
       console.log('failure')
       window.alert('Login failed, please try again');
+      // this.formData.setErrors({ unauthenticated: true })
     }
 
     // this.authService.login(data).subscribe(
