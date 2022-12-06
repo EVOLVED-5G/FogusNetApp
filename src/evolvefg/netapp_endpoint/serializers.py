b0c6fbfd80c0229626e2fba2f5e4379bcd9e4f11
katerinagiann <katerinagiann98@gmail.com>
2022-12-06 17:28:26 +0200

    Changes on env file

--- src/evolvefg/netapp_endpoint/serializers.py
+++ src/evolvefg/netapp_endpoint/serializers.py
@@ -30,29 +30,6 @@ class CellSerializer(serializers.HyperlinkedModelSerializer):
         model = Cell
         fields = ['id', 'cellId', 'latitude', 'longitude']
 
-# User serialezers
-# class RegisterSerializer(serializers.ModelSerializer):
-#     username = serializers.CharField(
-#         required=True,
-#         validators=[UniqueValidator(queryset=User.objects.all())]
-#     )
-
-#     class Meta:
-#         model = User
-#         fields = ["username","first_name", "last_name","email", "password", "password_confirm"]
-
-
-#     def create(self, validated_data):
-#         user = User.objects.create(
-#             username=validated_data['username'],
-#             email=validated_data['email'],
-#             first_name=validated_data['first_name'],
-#             last_name=validated_data['last_name']
-#         )
-#         user.set_password(validated_data['password'])
-#         user.save()
-#         return user
-
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
@@ -67,12 +44,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
     )
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password_confirm = serializers.CharField(write_only=True, required=True)
-    # password = serializers.CharField(
-    #     max_length=128,
-    #     min_length=8,
-    #     write_only=True
-    # )
-
     token = serializers.CharField(max_length=255, read_only=True)
     class Meta:
         model = User
