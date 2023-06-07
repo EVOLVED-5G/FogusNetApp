4e4abc2b8b617cdc63478d3ec4238be9b31354ac
Katerina Giannopoulou <katerinagiann98@gmail.com>
2023-06-07 13:45:09 +0300

    Changes in navbar

--- src/netappfe/src/assets/env.js
+++ src/netappfe/src/assets/env.js
@@ -2,7 +2,7 @@
   window["env"] = window["env"] || {};
   // Environment variables
   //@ts-ignore
-  window["env"]["backend"] = "192.168.1.20:8000";
+  window["env"]["backend"] = "192.168.1.27:8000";
   //@ts-ignore
-  window["env"]["frontend"] = "192.168.1.20:4200";
+  window["env"]["frontend"] = "192.168.1.27:4200";
 })(this);
