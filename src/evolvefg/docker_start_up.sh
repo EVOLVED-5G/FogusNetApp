86104ab3de79a019bc29c4d7d7732b9b6a03d4c2
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-07-13 13:10:05 +0200

    Change name

--- src/evolvefg/docker_start_up.sh
+++ src/evolvefg/docker_start_up.sh
@@ -8,7 +8,7 @@ jq -r .capif_host=\"$CAPIF_HOSTNAME\" capif_registration.json >> tmp.json && mv 
 jq -r .capif_http_port=\"$CAPIF_PORT_HTTP\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_https_port=\"$CAPIF_PORT_HTTPS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_callback_url=\"$CALLBACK_ADDRESS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
-evolved5g register-and-onboard-to-capif --config_file_full_path="/code/capif_registration.json" --environment="$ENVIRONMENT"
+evolved5g register-and-onboard-to-capif --config_file_full_path="/code/capif_registration.json" --environment="$ENVIRONMENT_MODE"
 
 # Make migrations
 echo "Making migrations"
